e0bc3f7115b0e41e7acf8d5b745227bf416fc99e
DobleA74 <andresandriani@gmail.com>
2024-01-24 14:53:58 -0300

    ej.2

---
+++ 02_Day_Data_types/02_day_starter/excerciseLevel2.js
@@ -0,0 +1,8 @@
+console.log('There is no exercise better for the heart than reaching down and lifting people up');
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same\
+ -- with charity you give love, so don't just give money but reach out your hand instead.");
+console.log(typeof '10' == 10);
+console.log(parseInt('10') == 10);
+console.log(parseFloat('9.8') == 10);
+console.log(Math.round(parseFloat('9.8')) == 10);
+console.log(('python and jargon').search('on'));
