b8fb2a6d5951e96b94d9b57f57aa03e5024cf13f
DobleA74 <andresandriani@gmail.com>
2024-01-24 20:29:22 -0300

    ej.2 mitad

--- 02_Day_Data_types/02_day_starter/excerciseLevel2.js
+++ 02_Day_Data_types/02_day_starter/excerciseLevel2.js
@@ -5,4 +5,10 @@ console.log(typeof '10' == 10);
 console.log(parseInt('10') == 10);
 console.log(parseFloat('9.8') == 10);
 console.log(Math.round(parseFloat('9.8')) == 10);
-console.log(('python and jargon').search('on'));
+console.log(('python and jargon').indexOf('on'));
+console.log(('python and jargon').lastIndexOf('on'));
+console.log(('I hope this course is not full of jargon.').includes('jargon'));
+const num = Math.floor(Math.random()*100);
+console.log(num);
+const num2 =Math.floor
+console.log(num2);
