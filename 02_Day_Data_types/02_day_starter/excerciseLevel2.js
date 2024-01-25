8623a86db0b0ae7563d29778d30c25f50ae0c379
DobleA74 <andresandriani@gmail.com>
2024-01-25 15:22:08 -0300

    temrinando parte 2 ej 2

--- 02_Day_Data_types/02_day_starter/excerciseLevel2.js
+++ 02_Day_Data_types/02_day_starter/excerciseLevel2.js
@@ -8,7 +8,15 @@ console.log(Math.round(parseFloat('9.8')) == 10);
 console.log(('python and jargon').indexOf('on'));
 console.log(('python and jargon').lastIndexOf('on'));
 console.log(('I hope this course is not full of jargon.').includes('jargon'));
-const num = Math.floor(Math.random()*100);
+const num = Math.floor(Math.random()*101);
 console.log(num);
-const num2 =Math.floor
+const num2 = Math.floor(Math.random()* 50 + 51); 
 console.log(num2);
+const num3 = Math.floor(Math.random() * 256); 
+console.log(num3);
+let string = 'JavaScript'
+let randomLetter = Math.floor(Math.random()*10);
+let selectedLetter = string[randomLetter]
+console.log(selectedLetter);
+console.log('1 1 1 1 1\n2 1 2 4 8 \n3 1 3 9 27 \n4 14 16 64 \n5 1 5 25 125');
+console.log(('You cannot end a sentence with because because because is a conjunction').substring(31,54));
