563466707800b230618cf1fa01a6d6cacb9fa950
DobleA74 <andresandriani@gmail.com>
2024-01-29 15:41:07 -0300

    ex 1 del dia 3

---
+++ 03_Day_Booleans_operators_date/03_day_starter/ex1.js
@@ -0,0 +1,25 @@
+let firstName = 'Andres';
+let lastName =  'Andriani';
+let country = 'Argentina';
+let city = 'Rosario';
+let age = 31;
+let isMarried = false;
+let year = 2024;
+console.log(typeof firstName);
+console.log(typeof '10' == 10);
+console.log(parseInt('9.8') == 10);
+
+console.log(age >= 30);
+console.log(isMarried == false);
+console.log(city == 'Rosario');
+
+console.log(age < 30);
+console.log(year < 2021);
+console.log(country == age);
+const now = new Date ();
+console.log(now);
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getHours());
+console.log(now.getMinutes());
