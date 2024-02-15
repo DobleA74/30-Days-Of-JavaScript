03699d41a137fd20e228579b4bd709e74c823db2
DobleA74 <andresandriani@gmail.com>
2024-02-15 15:30:19 -0300

    ejercitacion dia 3

--- 03_Day_Booleans_operators_date/03_day_starter/ex1.js
+++ 03_Day_Booleans_operators_date/03_day_starter/ex1.js
@@ -23,3 +23,40 @@ console.log(now.getMonth());
 console.log(now.getDate());
 console.log(now.getHours());
 console.log(now.getMinutes());
+
+// let base = 20;
+// let height = 10
+// let area = (0.5 * base * height)
+// console.log(area);
+
+//1.
+
+// let base = prompt('Enter number of base', 20);
+// let height = prompt('enter number of height',10);
+// let area = (0.5 * base * height);
+
+// //2.
+
+// let sideA = prompt('side A', '5');
+// let sideB = prompt('side B', 4);
+// let sideC = prompt('side C', 3);
+// let perimeter = (sideA, sideB, sideC);
+
+// //3.
+
+// let length = prompt('length',);
+// let width = prompt('width',);
+// let areaPerimeter = (2 *(length + width));
+
+// //4.
+
+// let radio = prompt('radio', );
+// let pi = 3.14
+// let areaCircle = (pi * radio * radio);
+// let circumference = (2 * pi * radio);
+
+if (firstName.length > 7) {
+    console.log('your name is long');
+}else{
+    console.log('your name is short');
+}
