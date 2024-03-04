9458799ef16f4fd7656846081a277a36e987726b
DobleA74 <andresandriani@gmail.com>
2024-03-04 15:54:14 -0300

    hasta el ejercicios 9, del dia 5

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,19 @@
 console.log(countries)
 alert('Open the browser console whenever you work on JavaScript')
 alert('Open the console and check if the countries has been loaded')
+
+const arr = Array();
+console.log(arr);
+
+const arr2 = Array(5).fill(0);
+console.log(arr2);
+console.log(arr2.length);
+console.log(arr2[1]);
+
+const mixedDataTypes = ['ascaban', 'Dario',' ese', 'Leti', 'raulo', 'por eso te digo'];
+console.log(mixedDataTypes.length);
+const itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple','IBM', 'Oracle', 'Amazon'];
+console.log(itCompanies);
+console.log(itCompanies.indexOf('Oracle'));
+console.log(itCompanies[0]);
+console.log(itCompanies[4]);
