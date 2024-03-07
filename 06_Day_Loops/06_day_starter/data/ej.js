9f002dd1ba2459ba4821922882f32c2564a775eb
DobleA74 <andresandriani@gmail.com>
2024-03-07 15:30:35 -0300

    haciendo ejercitaicon dia 6

---
+++ 06_Day_Loops/06_day_starter/data/ej.js
@@ -0,0 +1,74 @@
+const countriesNew = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+]
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+]
+
+const mernStack = [
+    'MongoDB',
+    'Express',
+    'React',
+    'Node'
+]
+for (let i = 0; i < 11; i++) {
+        console.log(i);
+}
+let w = 0;
+while ((w<=10)) {
+    console.log(w)
+    w++;
+}
+let d = 0;
+do{
+    console.log(d);
+    d++
+}while (d <=10) { 
+}
+for (let i = 10; i >= 0; i--) {
+    console.log(i);;
+}
+let r = 10;
+while ((r >= 0)) {
+    console.log(r)
+    r--;
+}
+
+const newArr =[]
+
+for (let i = 0; i < 7; i++) {
+    newArr.push('#')
+    console.log(newArr);
+}
+
+for (let i = 0; i <= 10; i++) {
+   
+   console.log(`${i} * ${i} = ${i*i}`); 
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${i} ${i**2} ${i**3}`);;
+}
+for (let i = 0; i <= 100; i++) {
+    if ((i % 2)= 0 ) {
+        console.log(i);
+    };
+    
+}
