eca4992ecb27f2b8b9061b03ff009dd4b62f585c
DobleA74 <andresandriani@gmail.com>
2024-03-19 15:42:13 -0300

    ejercitacion de loops con numero aleatorio

--- 06_Day_Loops/06_day_starter/data/ej.js
+++ 06_Day_Loops/06_day_starter/data/ej.js
@@ -43,7 +43,7 @@ do{
 }while (d <=10) { 
 }
 for (let i = 10; i >= 0; i--) {
-    console.log(i);;
+    console.log(i);
 }
 let r = 10;
 while ((r >= 0)) {
@@ -66,9 +66,35 @@ for (let i = 0; i <= 10; i++) {
 for (let i = 0; i <= 10; i++) {
     console.log(`${i} ${i**2} ${i**3}`);;
 }
-for (let i = 0; i <= 100; i++) {
-    if ((i % 2)= 0 ) {
-        console.log(i);
-    };
-    
+for (let num = 0; num <= 100; num++) {
+    if ( num % 2 == 0) {
+        console.log(num);
+    };   
 }
+for (let num2 = 0; num2 <= 100; num2++) {
+    if ( num2 % 2 !== 0) {
+        console.log(num2);
+    }; 
+}
+for (let numP = 0; numP <= 100; numP++) {
+    if ( numP % 2 == 1 &&
+         numP % numP == 0) {
+        console.log(numP);
+    }; 
+}
+total = 0
+for (let sum = 0; sum <= 100; sum++) {
+    total += sum
+    console.log(total);
+}
+totalEvens = 0;
+totalOdds = 0;
+for (let suma = 0; suma <= 100; suma++) {
+    if (suma % 2 ==0) {
+        totalEvens += suma;
+    }else{
+        totalOdds +=suma;
+    }
+}
+console.log(`The sum of all evens from 0 to 100 is ${totalEvens}. 
+And the sum of all odds from 0 to 100 is ${totalOdds}.`);
---
+++ 06_Day_Loops/06_day_starter/scripts/array.js
@@ -0,0 +1,18 @@
+
+
+
+for (let i = 0; i <= 5; i++) {
+    
+    newArr5.push(newArr5[i]*Math.floor(Math.random()*10))
+    
+    console.log(newArr5);
+}
+const numbers = [1, 2, 3, 4, 5]
+const newArrpp = []
+
+for(let i = 0; i < numbers.length; i++){
+  newArrpp.push( numbers[i] ** 2)
+
+}
+
+console.log(newArrpp)  // [1, 4, 9, 16, 25]
